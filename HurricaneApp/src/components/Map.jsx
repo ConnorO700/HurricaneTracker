e777298e89a077c22638063a1ce518d38413b7e8
= <=>
2025-10-02 10:07:24 -0400

    add start and end caps onto storm path

--- HurricaneApp/src/components/Map.jsx
+++ HurricaneApp/src/components/Map.jsx
@@ -1,9 +1,64 @@
-import React from 'react'
-
-function Map() {
+import React, {useEffect, useState} from 'react'
+import { ComposableMap, Geographies, Geography, Line, Sphere, Marker } from '@vnedyalk0v/react19-simple-maps';
+import startIcon from '../assets/s-start.png'
+import endIcon from '../assets/e-end.png'
+const geoUrl = 'https://unpkg.com/world-atlas@2/countries-110m.json';
+function Map({ coords }) {
 	return (
 		<>
-		map
+			<ComposableMap
+				projection="geoEqualEarth"
+				projectionConfig={{
+					scale: 147,
+					center: [0, 0],
+				}}
+				width={800}
+				height={500}
+			>
+				<Geographies geography={geoUrl}>
+					{({ geographies }) =>
+						geographies.map((geo) => (
+							<Geography
+								key={geo.rsmKey}
+								geography={geo}
+								style={{
+									default: { fill: '#D6D6DA', outline: 'none' },
+									hover: { fill: '#3e5bddff', outline: 'none' },
+									pressed: { fill: '#1a41f1ff', outline: 'none' },
+								}}
+							/>
+						))
+					}
+				</Geographies>
+				<Line
+					coordinates={coords}
+					stroke="#FF5533"
+					strokeWidth={3}
+					strokeLinecap="round"
+				/>
+				<Marker coordinates={coords[0] ?? [0,0]}>
+					<image
+            href={startIcon}
+            x={-7} // Adjust x and y for image centering/positioning
+            y={-7}
+            width="14" // Set image width
+            height="14" // Set image height
+            alt={"start"}
+          />
+				</Marker>
+				<Marker coordinates={coords[coords.length -1] ?? [0,0]}>
+					<image
+            href={endIcon}
+            x={-7} // Adjust x and y for image centering/positioning
+            y={-7}
+            width="14" // Set image width
+            height="14" // Set image height
+            alt={"end"}
+          />
+				</Marker>
+				
+				<Sphere stroke="#1a41f1ff" />
+			</ComposableMap>
 		</>
 	)
 }
--- HurricaneApp/src/pages/HurricaneView.jsx
+++ HurricaneApp/src/pages/HurricaneView.jsx
@@ -3,8 +3,7 @@ import Table from '../components/TableView'
 import { stormDetailColumns, tableCustomStyles } from '../consts/tableStormColumns'
 import ApiEndpoints from '../ApiEndpoints'
 import { useParams } from 'react-router-dom'
-import { ComposableMap, Geographies, Geography, Line } from '@vnedyalk0v/react19-simple-maps';
-const geoUrl = 'https://unpkg.com/world-atlas@2/countries-110m.json';
+import Map from '../components/Map'
 
 function HurricaneView() {
 	const { stormId } = useParams();
@@ -14,7 +13,7 @@ function HurricaneView() {
 		const fetchStorm = async () => {
 			var result = await ApiEndpoints.GetStormById(stormId)
 			setData(result);
-			const coords = result.hurricaneDetails.map((hd) =>{
+			const coords = result.hurricaneDetails.map((hd) => {
 				return [hd.longitude, hd.latitude];
 			})
 			setStormCoords(coords);
@@ -26,37 +25,7 @@ function HurricaneView() {
 			<div>
 
 				<Table title={`${stormData.name}'s Table`} data={stormData.hurricaneDetails} columns={stormDetailColumns} tableStyles={tableCustomStyles} />
-				<ComposableMap
-					projection="geoEqualEarth"
-					projectionConfig={{
-						scale: 147,
-						center: [0, 0],
-					}}
-					width={800}
-					height={500}
-				>
-					<Geographies geography={geoUrl}>
-						{({ geographies }) =>
-							geographies.map((geo) => (
-								<Geography
-									key={geo.rsmKey}
-									geography={geo}
-									style={{
-										default: { fill: '#D6D6DA', outline: 'none' },
-										hover: { fill: '#F53', outline: 'none' },
-										pressed: { fill: '#E42', outline: 'none' },
-									}}
-								/>
-							))
-						}
-					</Geographies>
-					<Line
-						coordinates={stormCoords}
-						stroke="#FF5533"
-						strokeWidth={3}
-						strokeLinecap="round"
-					/>
-				</ComposableMap>
+				<Map coords={stormCoords} />
 			</div>
 		</>
 	)
